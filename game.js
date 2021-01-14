c6f5fd64bad2bd5ddbca6e3c3beca3a5daaeb17f
Jakub Siemienowicz <jacobsiemienowicz@gmail.com>
2021-01-14 02:38:07 +0100

    Added zombies

--- game.js
+++ game.js
@@ -1,19 +1,20 @@
 // 0 = street block | 1 = building block
 //Level 1
-const levelOne = [0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,1,1,1,0,1,1,0,
-                  1,1,0,1,1,1,0,0,0,0,0,0,0,1,1,1,0,1,1,0,1,1,0,1,
-                  1,1,0,1,1,1,0,0,0,0,0,0,0,1,1,1,0,1,1,0,1,1,0,1,
-                  1,1,0,1,1,1,0,0,0,0,0,0,0,1,1,1,0,1,1,0,1,1,0,1,
-                  1,1,0,1,1,1,0,0,0,0,0,0,0,1,1,1,0,1,1,0,1,1,0,1,
-                  1,1,0,1,1,1,0,0,0,0,0,0,0,1,1,1,0,1,1,0,1,1,0,1,
-                  1,1,0,1,1,1,0,0,0,0,0,0,0,1,1,1,0,1,1,0,1,1,0,1,
-                  1,1,0,1,1,1,0,0,0,0,0,0,0,1,1,1,0,1,1,0,1,1,0,1,
-                  1,1,0,1,1,1,0,0,0,0,0,0,0,1,1,1,0,1,1,0,1,1,0,1,
-                  1,1,0,1,1,1,0,0,0,0,0,0,0,1,1,1,0,1,1,0,1,1,0,1,
-                  1,1,0,1,1,1,0,0,0,0,0,0,0,1,1,1,0,1,1,0,1,1,0,1,
-                  1,1,0,1,1,1,0,0,0,0,0,0,0,1,1,1,0,1,1,0,1,1,0,1,
-                  1,1,0,1,1,1,0,0,0,0,0,0,0,1,1,1,0,1,1,0,1,1,0,1,
-                  1,1,0,1,1,1,0,0,0,0,0,0,0,1,1,1,0,1,1,0,1,1,0,1,
+const levelOne = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                  1,1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,
+                  1,0,0,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,1,0,1,
+                  1,0,0,1,0,0,0,0,1,0,1,1,0,0,0,1,0,1,1,0,1,0,1,
+                  1,0,1,1,0,1,0,0,1,0,0,1,0,0,0,1,0,1,1,1,1,0,1,
+                  1,0,0,1,0,1,0,0,1,1,1,1,0,0,1,1,0,1,1,0,1,0,1,
+                  1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,1,1,0,1,0,1,
+                  1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,0,1,1,0,1,0,1,
+                  1,1,0,0,1,1,0,0,1,1,1,0,0,1,0,1,0,1,1,0,1,0,1,
+                  1,1,0,0,1,0,0,0,0,0,1,0,0,1,0,1,0,1,1,0,1,0,1,
+                  1,0,0,0,1,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,1,0,1,
+                  1,0,0,1,1,1,0,0,0,0,0,0,1,1,0,1,0,1,1,0,0,0,1,
+                  1,0,0,1,0,0,0,0,0,0,0,1,0,1,1,1,0,1,1,0,1,0,1,
+                  1,0,0,1,0,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,
+                  1,1,1,1,1,1,1,1,1,1,1,1,1,1
                   
 ]
 const game = document.getElementById("city")
@@ -35,7 +36,7 @@ levelOne.forEach((element) => {
     streetId++;
     // bombs 
     streetBlock.addEventListener("click", () => {
-      if (bombs<3) {
+      if (bombs<5) {
         streetBlock.infecteable = false
         streetBlock.className = "street-bombed"
         bombs++;
@@ -46,8 +47,10 @@ levelOne.forEach((element) => {
   else {
     const buldingBlock = document.createElement("div")
     buldingBlock.className = "building"
+    buldingBlock.id= "street-block-" + streetId;
     game.appendChild(buldingBlock)
     buldingBlock.infectable = false
+    streetId++;
   }
   // lineBreak++
   // if ((lineBreak - 9) % 10 == 10)  {
@@ -58,25 +61,69 @@ levelOne.forEach((element) => {
 // zombie
 const zombie = () => {
   //inicjuje pierwszego zombie
-  document.getElementById("street-block-0").infected = true;
+  document.getElementById("street-block-102").infected = true;
+  document.getElementById("street-block-102").newInfected = 0;
+  document.getElementById("street-block-102").className = "zombie";
   //Sprawdza wszytkie bloki na których mogą być zombie
-  let streetBlocks = Array.from(document.querySelectorAll('.street'))
+  let streetBlocks = Array.from(document.querySelectorAll('.zombie'))
   // Wypluwa tablice z wszystkimi kafelkami które są zombie
   const infectedZombies = streetBlocks.filter(isZombie => {
     return isZombie.infected == true;
   })
+  console.log("zainfekowane zombie: ")
   console.log(infectedZombies)
   
-  // Zarażanie sąsiadów 
-  let position = infectedZombies[0].id.slice(13)
-  console.log(position)
-  // górny blok
-  let positionTopNeighbor = position - 15
-  // prawy blok
-  let positionRightNeighbor = position + 1  
-  // dolny blok
-  let positionBottomNeighbor = position + 15 
-  // lewy blok 
-  let positionLeftNeighbor = position - 1
+
+  infectedZombies.forEach((zombie) =>{
+    let position = zombie.id.slice(13)
+    console.log(position)
+    // Zarażanie sąsiadów 
+    // górny blok
+    let positionTopNeighbor = Number(position) - 23;
+    let topNeighbor = document.getElementById("street-block-"+positionTopNeighbor)
+    if (topNeighbor.className == "street") {
+      topNeighbor.className = "zombie"
+      topNeighbor.newInfected = 0;
+      topNeighbor.infected = true;
+    }
+    // prawy blok
+    let positionRightNeighbor = Number(position)+ 1
+    let rightNeighbor = document.getElementById("street-block-"+positionRightNeighbor)
+    if (rightNeighbor.className == "street") {
+      rightNeighbor.className = "zombie"
+      rightNeighbor.newInfected = 0;
+      rightNeighbor.infected = true;
+    }
+    // dolny blok
+    let positionBottomNeighbor = Number(position) + 23;
+    let bottomNeighbor = document.getElementById("street-block-"+positionBottomNeighbor)
+    if (bottomNeighbor.className == "street") {
+      bottomNeighbor.className = "zombie"
+      bottomNeighbor.newInfected = 0;
+      bottomNeighbor.infected = true;
+    }
+    // lewy blok 
+    let positionLeftNeighbor = Number(position) - 1
+    let leftNeighbor = document.getElementById("street-block-"+positionLeftNeighbor)
+    if (leftNeighbor.className == "street") {
+      leftNeighbor.className = "zombie"
+      leftNeighbor.newInfected = 0;
+      leftNeighbor.infected = true;
+    }
+    if (gameLoopCounter<10) {
+      gameLoop()
+      time = time + 300
+    }
+  })
   //for each które przejdzie przez infectedZombies i zarazi sąsiadów
 }
+
+let gameLoopCounter = 0
+time = 500
+
+const gameLoop = () => window.setTimeout(gameLoop => {
+  zombie()
+  gameLoopCounter++
+}, time)
+
+document.getElementById("start-attack").addEventListener('click', zombie)
